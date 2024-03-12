50af285084dc4c309f1025c4d2806011190da0c6
maki <okuda.maki.115@gmail.com>
2024-03-12 11:05:41 +0900

    initilie

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/maki/ros2_ws/src/ros2humble_tutorials/cpp_pubsub
+# Build directory: /home/maki/ros2_ws/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
